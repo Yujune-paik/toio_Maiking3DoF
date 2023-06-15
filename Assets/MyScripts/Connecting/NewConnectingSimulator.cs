b1557e55aff5cf979e4e86c63998f437174f38c3
Yujune-Paik <al21821@shibaura-it.ac.jp>
2023-06-15 21:20:25 +0900

    NewConnectingSimulator.csを完成させたよ．

--- Assets/MyScripts/Connecting/NewConnectingSimulator.cs
+++ Assets/MyScripts/Connecting/NewConnectingSimulator.cs
@@ -5,183 +5,122 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using toio;
-using System.Threading.Tasks;
 
 public class NewConnectingSimulator : MonoBehaviour
 {
     public Text label;
 
     CubeManager cm;
-    public ConnectType connectType = ConnectType.Real;
+    public ConnectType connectType;
 
     int phase = 0;
-    int check = 0;
 
-    Vector2 PosCubeLeft = new Vector2(0, 0);
-    Vector2 PosCubeRight = new Vector2(0, 0);
+    Vector2 PosLeft = new Vector2(0, 0);
+    Vector2 PosRight = new Vector2(0, 0);
 
-    int AngleCubeLeft = 0;
-    int AngleCubeRight = 0;
+    string CubeLeft = "Cube_left";
+    string CubeRight = "Cube_right";
 
-    int L_cube=50;
+    int AngleLeft = 0;
+
+    int L = 50;
 
     public int connectNum = 2;
 
-    Dictionary<int, string> toio_dict = new Dictionary<int, string>();
-    
+    float timeSinceLastOrder = 0f;
+    float orderInterval = 0.1f;
+
     async void Start()
     {
-        using (var sr = new StreamReader("Assets/toio_number.csv"))
-        {
-            while (!sr.EndOfStream)
-            {
-                var line = sr.ReadLine();
-                var values = line.Split(',');
-                toio_dict.Add(int.Parse(values[0]), values[1]);
-            }
-        }
-
         cm = new CubeManager(connectType);
-        // キューブの複数台接続
-        await ConnectToioCubes();
-    }
-
-    async Task ConnectToioCubes()
-    {
-        while (cm.syncCubes.Count < connectNum)
-        {
-            await cm.MultiConnect(connectNum);
-        }
-        // 接続台数をコンソールに表示する
-        Debug.Log(cm.syncCubes.Count);
+        await cm.MultiConnect(connectNum);
     }
 
     void Update()
     {
-        if (cm.synced)
+        timeSinceLastOrder += Time.deltaTime;
+
+        if (cm.synced && timeSinceLastOrder >= orderInterval)
         {
+            timeSinceLastOrder = 0f; 
+
             foreach(var navigator in cm.syncNavigators)
-            {
-                if(check == 0)
+            {   
+                // 1. まずはCubeRightの移動後の位置と角度を計算する
+                if(phase == 0)
                 {
-                    if(navigator.cube.localName == "Cube_left" && navigator.cube.x != 0 && navigator.cube.y != 0)
+                    if(navigator.cube.localName == CubeLeft && navigator.cube.x != 0 && navigator.cube.y != 0)
                     {
-                        PosCubeLeft = new Vector2(navigator.cube.x, navigator.cube.y);
-                        AngleCubeLeft = navigator.cube.angle;
-                        check += 1;
-                        PosCubeRight = CalculateNewPosition(PosCubeLeft, AngleCubeLeft, L_cube);
-                        Debug.Log("PosCubeRight: " + PosCubeRight.x + ", " + PosCubeRight.y);
+                        PosLeft = new Vector2(navigator.cube.x, navigator.cube.y);
+                        AngleLeft = navigator.cube.angle;
+                        PosRight = CalculateNewPosition(PosLeft, AngleLeft, L);
+                        Debug.Log("PosRight: " + PosRight.x + ", " + PosRight.y);
+                        phase += 1;
                     }
                 }
-                else
-                {
-                    if(phase == 0)
-                    {
-                        if(navigator.cube.localName == "Cube_right")
-                        {
-                            var mv = navigator.Navi2Target(PosCubeRight.x, PosCubeRight.y, maxSpd:20, rotateTime:1000,tolerance:15).Exec();
-                            if(mv.reached)
-                            {
-                                phase += 1;
-                                Debug.Log("phase0");
-                            }
-                        }
-                    }
 
-                    else if(phase == 1)
+                // 2. CubeRightの座標へ移動させる
+                else if(phase == 1) 
+                {
+                    if(navigator.cube.localName == CubeRight)
                     {
-                        if(navigator.cube.localName == "Cube_right")
+                        var mv = navigator.Navi2Target(PosRight.x, PosRight.y, maxSpd:50).Exec();
+                        if(mv.reached)
                         {
-                            Movement mv = navigator.handle.Rotate2Deg(AngleCubeLeft, rotateTime:2500, tolerance:0.1).Exec();
-                            if(mv.reached)
-                            {
-                                phase += 1;
-                                Debug.Log("phase1");
-                            }
+                            phase += 1;
+                            Debug.Log("phase1");
                         }
                     }
-                    else if(phase == 2)
+                }
+
+                // 3-1. 指定した角度まで回転
+                else if(phase == 2) 
+                {
+                    if(navigator.cube.localName == CubeRight)
                     {
-                        if(navigator.cube.localName == "Cube_right")
+                        int angle_diff = AngleLeft - navigator.cube.angle;
+                        if(Math.Abs(angle_diff) < 5)
                         {
-                            navigator.handle.Move(-15, 0, 1000);
                             phase += 1;
                             Debug.Log("phase2");
                         }
-                        // Debug.Log("phase2 : Cube_Angle: " + navigator.cube.angle);
+                        else if(angle_diff > 0)
+                        {
+                            navigator.handle.Move(0, 10, 20);
+                        }
+                        else
+                        {
+                            navigator.handle.Move(0, -10, 20);
+                        }
                     }
-                    // else if(phase == 1)
-                    // {
-                    //     if(navigator.cube.localName == "Cube_right")
-                    //     {
-                    //         var angleDifference = Math.Abs(AngleCubeLeft - navigator.cube.angle);
-                    //         if(angleDifference <= 3)
-                    //         {
-                    //             phase += 1;
-                    //             Debug.Log("phase1");
-                    //         }
-                    //         else
-                    //         {
-                    //             if(AngleCubeLeft < navigator.cube.angle)
-                    //             {
-                    //                 navigator.handle.Move(0, -5, 10);
-                    //             }
-                    //             else
-                    //             {
-                    //                 navigator.handle.Move(0, 5, 10);
-                    //             }
-                    //         }
-                    //     }
-                    // }
-                    // else if(phase == 2)
-                    // {
-                    //     if(navigator.cube.localName == "Cube_right")
-                    //     {
-                    //         var distance = Vector2.Distance(PosCubeRight, new Vector2(navigator.cube.x, navigator.cube.y));
-                    //         if(distance <= 30)
-                    //         {
-                    //             phase += 1;
-                    //             Debug.Log("phase2");
-                    //         }
-                    //         else
-                    //         {
-                    //             if(distance > 0)
-                    //             {
-                    //                 navigator.handle.Move(-5, 0, 10);
-                    //             }
-                    //             else
-                    //             {
-                    //                 navigator.handle.Move(5, 0, 10);
-                    //             }
-                    //         }
-                    //     }
-                    //     // Debug.Log("phase2 : Cube_Angle: " + navigator.cube.angle);
-                    // }
+                }
 
+                // 3-2. 指定した距離まで移動
+                else if(phase == 3) 
+                {
+                    if(navigator.cube.localName == CubeRight)
+                    {
+                        float distance = Vector2.Distance(new Vector2(navigator.cube.x, navigator.cube.y), PosLeft);
+                        if(distance < 25)
+                        {
+                            phase += 1;
+                            Debug.Log("phase3");
+                        }
+                        else
+                        {
+                            navigator.handle.Move(-10, 0, 20);
+                        }
+                    }
                 }
             }
 
             string text = "";
             foreach (var cube in cm.syncCubes)
             {
-                if(cube.localName == "Cube_right")
-                {
-                    text += "Cube_right： (" + cube.x + "," + cube.y + "," + cube.angle + ")\n";
-                    PosCubeRight = cube.pos;
-                    AngleCubeRight = cube.angle;
-                }
-                else if(cube.localName == "Cube_left")
-                {
-                    text += "Cube_left： (" + cube.x + "," + cube.y + "," + cube.angle + ")\n";
-                    PosCubeLeft = cube.pos;
-                    
-                }
+                if(cube.localName == CubeRight) text += "CubeRight: ";
+                else if(cube.localName == CubeLeft) text += "CubeLeft: ";
 
-                // CubeRightとCubeLeftの距離と角度の差を計算
-                var distance = Vector2.Distance(PosCubeRight, PosCubeLeft);
-                var angleDifference = Math.Abs(AngleCubeLeft - AngleCubeRight);
-                text += "distance: " + distance + "\n";
-                text += "angleDifference: " + angleDifference + "\n";
+                text += "(" + cube.x + "," + cube.y + "," + cube.angle + ")\n";
             }
             if (text != "") this.label.text = text;
         }
